aeff35c691ab7ca56a2cc1e4611f6ab65dc930dc
Flavio Daniel <flaviodanielgs@gmail.com>
2023-04-14 23:55:54 -0300

    Ajustar estrutura / add SASS

---
+++ src/App/Calculator/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss"
+
+export default function Calculator() {
+  return (
+    <div>
+      <div>calculadora</div>
+    </div>
+  );
+}
---
+++ src/App/HomeScreen/index.tsx
@@ -0,0 +1,10 @@
+import Calculator from "../Calculator";
+import styles from "./styles.module.scss";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
